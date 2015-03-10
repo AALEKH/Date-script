e7d218ac373270e81048fcbb00c5c47b5ec33a5e
Aalekh Nigam <aalekh1993@rediffmail.com>
2015-03-10 13:53:04 +0530

    Update TimeDate.java

--- TimeDate.java
+++ TimeDate.java
@@ -4,34 +4,41 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import org.joda.time.DateTime;
+import org.joda.time.DateTime.*;
 import org.joda.time.*;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormat;
 
+import java.text.SimpleDateFormat;
+
 class TestDate {
 
     public static void main(String[] args) {   
-    Calendar c = new GregorianCalendar();
-    String dateTime = "11/15/2013 13:35:00";
-     // Format for input
-    DateTimeFormatter dtf = DateTimeFormat.forPattern("MM/dd/yyyy kk:mm:ss");
-     // Parsing the date
-    DateTime jodatime = dtf.parseDateTime(dateTime);
-    c.set(Calendar.HOUR_OF_DAY, Integer.parseInt(jodatime.toString("k")));
-    c.set(Calendar.MINUTE, Integer.parseInt(jodatime.toString("m")));
-    c.set(Calendar.SECOND, Integer.parseInt(jodatime.toString("s")));
-    Date d = c.getTime();
+        Calendar c = new GregorianCalendar();
+        String dateTime = "11/15/2013 13:35:00";
+         // Format for input
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("MM/dd/yyyy kk:mm:ss");
+         // Parsing the date
+        DateTime jodatime = dtf.parseDateTime(dateTime);
 
-    System.out.println("Start point: " + jodatime.toString());
-    System.out.println("Nearest whole minute: " + toNearestWholeMinute(d));
-    System.out.println("Nearest whole hour: " + toNearestWholeHour(d));
-    DateTime a = toNearestWholeMinute(d);
-    DateTime b = toNearestWholeHour(d);
-    System.out.println("Current hours:" + a.getHourOfDay());
-    System.out.println("Nearest hour:" + b.getHourOfDay());
+        c.set(Calendar.MONTH, Integer.parseInt(jodatime.toString("M")));
+        c.set(Calendar.DAY_OF_MONTH, Integer.parseInt(jodatime.toString("d")));
+        c.set(Calendar.YEAR, Integer.parseInt(jodatime.toString("y")));        
+        c.set(Calendar.HOUR_OF_DAY, Integer.parseInt(jodatime.toString("k")));
+        c.set(Calendar.MINUTE, Integer.parseInt(jodatime.toString("m")));
+        c.set(Calendar.SECOND, Integer.parseInt(jodatime.toString("s")));
+        java.util.Date d = c.getTime();
+        System.out.println("Date" + d);
+        System.out.println("Start point: " + jodatime.toString());
+        System.out.println("Nearest whole minute: " + toNearestWholeMinute(d));
+        System.out.println("Nearest whole hour: " + toNearestWholeHour(d));
+        DateTime a = toNearestWholeHour(d);
+        System.out.println(Integer.parseInt(a.toString("M")));
+        System.out.println(Integer.parseInt(a.toString("d")));
+        System.out.println(Integer.parseInt(a.toString("y")));
     }
 
-    static DateTime toNearestWholeMinute(Date d) {
+    static DateTime toNearestWholeMinute(java.util.Date d) {
     Calendar c = new GregorianCalendar();
     c.setTime(d);
 
@@ -44,7 +51,7 @@ class TestDate {
     return dateTime;
     }
 
-    static DateTime toNearestWholeHour(Date d) {
+    static DateTime toNearestWholeHour(java.util.Date d) {
     Calendar c = new GregorianCalendar();
     c.setTime(d);
 
@@ -59,10 +66,3 @@ class TestDate {
     }
 
 }
-
-/*
-/*
-    DateTime dateTime = new DateTime(c.getTime());
-    DateFormat outputformat = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
-    String aa = (String) outputformat.format(dateTime);
-    */
